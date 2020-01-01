a6de07a5b6b94ba800922e7eed1dcc5ea2afd248
Rafael Queiroz <rafaelfqf@gmail.com>
2020-01-01 11:22:11 -0300

    Bake plugin

---
+++ src/Controller/AppController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CakeGraphql\Controller;
+
+use App\Controller\AppController as BaseController;
+
+class AppController extends BaseController
+{
+}
---
+++ src/Plugin.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CakeGraphql;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin for CakeGraphql
+ */
+class Plugin extends BasePlugin
+{
+}
